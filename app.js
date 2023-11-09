568e8117549eb8931ceaf44e4251f89d0f2d0dae
Hijikata1869 <liv.knk@gmail.com>
2023-11-09 20:33:27 +0900

    objectの型指定

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else
-        return result;
-}
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = "Result";
-add(number1, number2, printResult, resultPhrase);
+var person = {
+    name: "daichi",
+    age: 29
+};
+console.log(person.name);
