df570ffff137b94c1f4188092bfa7db6a35f0aba
Hijikata1869 <liv.knk@gmail.com>
2023-11-11 20:51:47 +0900

    enum型について学んだ

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
+// const person: {
+//   name: string;
+//   age: number;
+// }
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "daichi",
+//   age: 29,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "ADMIN";
+    Role[Role["READ_ONLY"] = 100] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 200] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: "daichi",
-    age: 29
+    age: 29,
+    hobbies: ["Sports", "Cooking"],
+    role: Role.ADMIN
 };
-console.log(person.name);
+console.log(Role);
