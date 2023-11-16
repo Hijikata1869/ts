f195f67cde00648b03fe6697a50f4c1cfea8f73a
Hijikata1869 <liv.knk@gmail.com>
2023-11-16 13:12:20 +0900

    レストパラメータ

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-const add = (a: number, b: number = 1) => {
-  return a + b;
-};
+// const add = (a: number, b: number = 1) => {
+//   return a + b;
+// };
 
-const printOutput: (output: string | number | string[]) => void = (output) =>
-  console.log(output);
+// const printOutput: (output: string | number | string[]) => void = (output) =>
+//   console.log(output);
 
 const hobbies = ["Sports", "Cooking"];
 const activeHobbies = ["Hiking"];
@@ -19,4 +19,11 @@ const copiedPerson = {
   ...person,
 };
 
-console.log(copiedPerson);
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const addedNumbers = add(5, 2, 3);
+console.log(addedNumbers);
