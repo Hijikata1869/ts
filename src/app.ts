fe48accfcf65c3b9d387bde209c42fc20f3d2d8d
Hijikata1869 <liv.knk@gmail.com>
2023-12-06 21:33:11 +0900

    ジェネリクス導入部分が終了

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 const names: Array<string> = []; // string[]
 // names[0].split(" ");
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
+const promise: Promise<string> = new Promise((resolve) => {
   setTimeout(() => {
     resolve("終わりました！");
   }, 2000);
 });
 
 promise.then((data) => {
-  data.split(" ");
+  console.log(data);
 });
