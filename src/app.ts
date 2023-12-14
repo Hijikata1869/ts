a1273f14ecdfb48ef9aa337e07290a3a57ad9fed
Hijikata1869 <liv.knk@gmail.com>
2023-12-14 18:01:18 +0900

    generic関数

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
-const names: Array<string> = []; // string[]
-// names[0].split(" ");
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
 
-const promise: Promise<string> = new Promise((resolve) => {
-  setTimeout(() => {
-    resolve("終わりました！");
-  }, 2000);
-});
+const mergedObj = merge({ name: "Max" }, { age: 30 });
 
-promise.then((data) => {
-  console.log(data);
-});
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "値がありません。";
+  if (element.length > 0) {
+    descriptionText = "値は" + element.length + "個です。";
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe([]));
