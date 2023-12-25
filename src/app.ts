373dc3ab6b5a98c7779e6827eb666c39089fd65f
Hijikata1869 <liv.knk@gmail.com>
2023-12-26 03:34:27 +0900

    ジェネリッククラス

--- src/app.ts
+++ src/app.ts
@@ -23,4 +23,24 @@ function extractAndConvert<T extends object, U extends keyof T>(
   return "Value: " + obj[key];
 }
 
-console.log(extractAndConvert({ name: "Max" }, "name"));
+class DataStorage<T> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStrage = new DataStorage();
+textStrage.addItem("data1");
+textStrage.addItem("data2");
+textStrage.removeItem("data2");
+console.log(textStrage.getItems());
