4ef8e40aec5f6a53378da98e42c0fc00ab5aa492
Hijikata1869 <liv.knk@gmail.com>
2023-12-29 19:27:44 +0900

    アクセサとパラメータのデコレータ

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 function Logger(logString: string) {
+  console.log("Logger ファクトリ");
   return function (constructor: Function) {
     console.log(logString);
     console.log(constructor);
@@ -6,7 +7,9 @@ function Logger(logString: string) {
 }
 
 function WithTemplate(template: string, hookId: string) {
+  console.log("template ファクトリ");
   return function (constructor: any) {
+    console.log("テンプレートを表示");
     const hookEl = document.getElementById(hookId);
     const p = new constructor();
     if (hookEl) {
@@ -16,16 +19,69 @@ function WithTemplate(template: string, hookId: string) {
   };
 }
 
-// @Logger("ログ出力中 - PERSON")
-@WithTemplate("<h1>Personオブジェクト</h1>", "app")
+// @Logger("ログ出力中")
+// @WithTemplate("<h1>Personオブジェクト</h1>", "app")
 class Person {
   name = "Max";
 
   constructor() {
-    console.log("Personオブジェクトを作成中・・・");
+    // console.log("Personオブジェクトを作成中・・・");
   }
 }
 
 const pers = new Person();
 
-console.log(pers);
+// console.log(pers);
+
+// ---
+
+function Log(target: any, propertyName: string | symbol) {
+  console.log("Property デコレータ");
+  console.log(target, propertyName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Accessor デコレータ");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log3(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Method デコレータ");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log4(target: any, name: string | symbol, position: number) {
+  console.log("Param デコレータ");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("不正な価格です - 0以下は設定できません");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  @Log3
+  getPriceWithTax(tax: number, @Log4 tax2: number) {
+    return this._price * (1 + tax + tax2);
+  }
+}
