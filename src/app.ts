d89db2d609238aaec3d829687400c5300e8ce047
Hijikata1869 <liv.knk@gmail.com>
2024-01-30 18:37:38 +0900

    必要なファイルに必要な依存関係を追加

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@
 /// <reference path="models/project.ts" />
 /// <reference path="state/project-state.ts" />
 /// <reference path="util/validation.ts" />
-/// <reference path="decorators/decorator.ts" />
+/// <reference path="decorators/autobind.ts" />
 /// <reference path="components/project-input.ts" />
 /// <reference path="components/project-list.ts" />
 
