9e803f156f50f0ffdfe32b989b921f9ee520d1d8
Hijikata1869 <liv.knk@gmail.com>
2024-01-30 19:18:58 +0900

    exモジュールを利用

--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,82 +1,81 @@
-/// <reference path="base-component.ts" />
-/// <reference path="../decorators/autobind.ts" />
-/// <reference path="../state/project-state.ts" />
-/// <reference path="../models/project.ts" />
-/// <reference path="../models/drag-drop.ts" />
+import { DragTarget } from "../models/drag-drop.js";
+import { Project, ProjectStatus } from "../models/project.js";
+import { Component } from "./base-component.js";
+import { autobind } from "../decorators/autobind.js";
+import { projectState } from "../state/project-state.js";
+import { ProjectItem } from "./project-item.js";
 
-namespace App {
-  // ProjectList Class
-  export class ProjectList
-    extends Component<HTMLDivElement, HTMLElement>
-    implements DragTarget
-  {
-    assignedProjects: Project[];
+// ProjectList Class
+export class ProjectList
+  extends Component<HTMLDivElement, HTMLElement>
+  implements DragTarget
+{
+  assignedProjects: Project[];
 
-    constructor(private type: "active" | "finished") {
-      super("project-list", "app", false, `${type}-projects`);
-      this.assignedProjects = [];
+  constructor(private type: "active" | "finished") {
+    super("project-list", "app", false, `${type}-projects`);
+    this.assignedProjects = [];
 
-      this.configure();
-      this.renderContent();
-    }
+    this.configure();
+    this.renderContent();
+  }
 
-    @autobind
-    dragOverHandler(event: DragEvent) {
-      if (event.dataTransfer && event.dataTransfer.types[0] === "text/plain") {
-        // event.preventDefaultを追加することによって、javascriptではこのドラッグ&ドロップだけが許可されるという動作になる。なので、この後のドロップイベントはドラッグオーバーハンドラでpreventDefaultが呼び出された要素に対してだけトリガーされる。これは次のように考えることができる。デフォルトのjavascriptのドラッグ&ドロップのイベントはドロップを許可しない。原則的には禁止している。なので、このsectionのHTMl要素、ProjectListの上にドラッグオーバーされたときにはpreventDefautを呼び出して、ドロップを許可する必要がある。なのでドラッグオーバーハンドラでpreventDefaultを呼び出すと、ユーザーがドロップしたときに、ドロップハンドラが呼び出されるということになる。これをしない場合はドロップハンドラは呼び出されない。ドロップイベントが起きない。
-        event.preventDefault();
-        const listEl = this.element.querySelector("ul")!;
-        listEl.classList.add("droppable");
-      }
-    }
-
-    @autobind
-    dropHandler(event: DragEvent) {
-      const prjId = event.dataTransfer!.getData("text/plain");
-      projectState.moveProject(
-        prjId,
-        this.type === "active" ? ProjectStatus.Active : ProjectStatus.Finished
-      );
-    }
-
-    @autobind
-    dragLeaveHandler(_: DragEvent) {
+  @autobind
+  dragOverHandler(event: DragEvent) {
+    if (event.dataTransfer && event.dataTransfer.types[0] === "text/plain") {
+      // event.preventDefaultを追加することによって、javascriptではこのドラッグ&ドロップだけが許可されるという動作になる。なので、この後のドロップイベントはドラッグオーバーハンドラでpreventDefaultが呼び出された要素に対してだけトリガーされる。これは次のように考えることができる。デフォルトのjavascriptのドラッグ&ドロップのイベントはドロップを許可しない。原則的には禁止している。なので、このsectionのHTMl要素、ProjectListの上にドラッグオーバーされたときにはpreventDefautを呼び出して、ドロップを許可する必要がある。なのでドラッグオーバーハンドラでpreventDefaultを呼び出すと、ユーザーがドロップしたときに、ドロップハンドラが呼び出されるということになる。これをしない場合はドロップハンドラは呼び出されない。ドロップイベントが起きない。
+      event.preventDefault();
       const listEl = this.element.querySelector("ul")!;
-      listEl.classList.remove("droppable");
+      listEl.classList.add("droppable");
     }
+  }
 
-    configure() {
-      this.element.addEventListener("dragover", this.dragOverHandler);
-      this.element.addEventListener("drop", this.dropHandler);
-      this.element.addEventListener("dragleave", this.dragLeaveHandler);
+  @autobind
+  dropHandler(event: DragEvent) {
+    const prjId = event.dataTransfer!.getData("text/plain");
+    projectState.moveProject(
+      prjId,
+      this.type === "active" ? ProjectStatus.Active : ProjectStatus.Finished
+    );
+  }
 
-      projectState.addListener((projects: Project[]) => {
-        const relevantProjects = projects.filter((prj) => {
-          if (this.type === "active") {
-            return prj.status === ProjectStatus.Active;
-          }
-          return prj.status === ProjectStatus.Finished;
-        });
-        this.assignedProjects = relevantProjects;
-        this.renderProjects();
+  @autobind
+  dragLeaveHandler(_: DragEvent) {
+    const listEl = this.element.querySelector("ul")!;
+    listEl.classList.remove("droppable");
+  }
+
+  configure() {
+    this.element.addEventListener("dragover", this.dragOverHandler);
+    this.element.addEventListener("drop", this.dropHandler);
+    this.element.addEventListener("dragleave", this.dragLeaveHandler);
+
+    projectState.addListener((projects: Project[]) => {
+      const relevantProjects = projects.filter((prj) => {
+        if (this.type === "active") {
+          return prj.status === ProjectStatus.Active;
+        }
+        return prj.status === ProjectStatus.Finished;
       });
-    }
+      this.assignedProjects = relevantProjects;
+      this.renderProjects();
+    });
+  }
 
-    renderContent() {
-      const listId = `${this.type}-projects-list`;
-      this.element.querySelector("ul")!.id = listId;
-      this.element.querySelector("h2")!.textContent =
-        this.type === "active" ? "実行中プロジェクト" : "完了プロジェクト";
-    }
+  renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent =
+      this.type === "active" ? "実行中プロジェクト" : "完了プロジェクト";
+  }
 
-    private renderProjects() {
-      const listEl = document.getElementById(
-        `${this.type}-projects-list`
-      )! as HTMLUListElement;
-      listEl.innerHTML = "";
-      for (const prjItem of this.assignedProjects) {
-        new ProjectItem(listEl.id, prjItem);
-      }
+  private renderProjects() {
+    const listEl = document.getElementById(
+      `${this.type}-projects-list`
+    )! as HTMLUListElement;
+    listEl.innerHTML = "";
+    for (const prjItem of this.assignedProjects) {
+      new ProjectItem(listEl.id, prjItem);
     }
   }
 }
