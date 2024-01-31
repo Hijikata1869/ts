c8b08d7ee53231acfe71c6b677e7c6d3dd3f79db
Hijikata1869 <liv.knk@gmail.com>
2024-01-31 18:55:22 +0900

    Axios &住所の座標取得

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+import "dotenv/config";
+require("dotenv").config();
+
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+
+// 意味合いの解説。戻り値はオブジェクトで、resultsというフィールドがある。これは、オブジェクトの配列。このオブジェクトはgeometryというキーを持っている。geometryはオブジェクト。そのオブジェクトはlocationというキーを持っている。このlocationもオブジェクト。この中にlat, lngのキーがあり、どちらもnumber型。
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${apiKey}`
+    )
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("座標を取得できませんでした。");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
+}
+form.addEventListener("submit", searchAddressHandler);
