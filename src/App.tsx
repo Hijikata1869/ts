abe4d790a0b7a69f73845c1f369eaaf6b1dfe65f
Hijikata1869 <liv.knk@gmail.com>
2024-02-04 18:30:28 +0900

    typescriptにおけるreactのstateの利用の仕方、型定義

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function App() {
+import TodoList from './components/TodoList'
+import NewTodo from './components/NewTodo';
+import { Todo } from './todo.model'
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const todoAddHandler = (text: string) => {
+    setTodos(prevTodos => [...prevTodos, {id: Math.random().toString(), text: text}]);
+  }
   return (
     <div className="App">
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos}/>
     </div>
   );
 }
